94bb4bd1d5ca5c8610d7c6fe5de562a022f47c17
unknown <wgraham@galacticedgeenterprises.com>
2023-12-03 18:26:27 -0600

    Added first recipe modification functionality

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from recipe import bp as recipe
 
 app = Flask(__name__)
 
-app.register_blueprint(recipe, url_prefix = 'recipe')
+app.register_blueprint(recipe, url_prefix = '/recipe')
--- recipe.py
+++ recipe.py
@@ -1,3 +1,10 @@
 from flask import Blueprint
+from flask import render_template
 
 bp = Blueprint('recipe', __name__, url_prefix = '')
+
+@bp.route('/edit')
+@bp.route('/<id>/edit')
+def recipe_edit(id = None):
+    page = render_template('recipe_edit.j2')
+    return page
