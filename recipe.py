a3e8bc5e2651da7285b20b1059da9de5b4d08a3f
unknown <wgraham@galacticedgeenterprises.com>
2023-12-09 12:40:50 -0600

    Added simplified version of expected functionality to form.

--- recipe.py
+++ recipe.py
@@ -1,10 +1,17 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
 
 bp = Blueprint('recipe', __name__, url_prefix = '')
 
-@bp.route('/edit')
+@bp.route('/edit', methods = ['GET'])
 @bp.route('/<id>/edit')
 def recipe_edit(id = None):
-    page = render_template('recipe_edit.j2')
+    recipe = None;
+    page = render_template('recipe_edit.j2', recipe = recipe)
     return page
+
+@bp.route('/edit', methods = ['POST'])
+def recipe_store(id = None):
+    content = request.form
+    return content
