15637dfe429bd746b3c5c59a5f198fd51d2bae27
unknown <wgraham@galacticedgeenterprises.com>
2024-01-09 09:28:29 -0600

    Added comments and prepped for additional services

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 from recipe import bp as recipe
 from menu import bp as menu
 
-app = Flask(__name__)
+# DataManager Integrations Setup
 
+# Flask Website Setup
+app = Flask(__name__)
 app.register_blueprint(recipe, url_prefix = '/recipe')
 app.register_blueprint(menu, url_prefix = '/menu')
